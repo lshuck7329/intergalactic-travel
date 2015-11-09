48946edd64f5e1e3d289bd5ad470d824de4f553b
student3 <student3@jh-mbair-158-18.d214.org>
2015-11-09 14:39:18 -0600

    added images

--- intergalacticTravelHackWitch/imageViewController.swift
+++ intergalacticTravelHackWitch/imageViewController.swift
@@ -10,15 +10,18 @@ import UIKit
 
 class imageViewController: UIViewController {
 
+    @IBOutlet weak var starImageView: UIImageView!
     var isBlueStar: Bool?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         if isBlueStar! {
             self.view.backgroundColor = UIColor.blueColor()
+            starImageView.image = UIImage(named: "blue star")
         }
         else {
             self.view.backgroundColor = UIColor.redColor()
+            starImageView.image = UIImage(named: "red dwarf")
         }
     }
 
